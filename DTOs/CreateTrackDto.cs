e50ec285e20f7a7c2585ffbef87254d2acabe690
Seb Quelcutti <squelcutti@gmail.com>
2021-01-08 13:40:55 +0000

    Added Track to back end

---
+++ DTOs/CreateTrackDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MindYourMoodWeb.DTOs
+{
+    public class CreateTrackDto
+    {
+        public int PlaylistId { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public int OrderNumber { get; set; } = 0;
+        public string Uri { get; set; }
+        public float Duration { get; set; } = 0.0f;
+    }
+}
